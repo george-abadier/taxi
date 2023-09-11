0710c69f459b53db6fbd582564a397a850bdcb99
george-abadier <gorgeabadier@gmail.com>
2023-09-11 23:26:14 +0200

    a

--- statics/frontend/js/home.js
+++ statics/frontend/js/home.js
@@ -11,14 +11,14 @@ var translateObject = {
     Our drivers are specially qualified who meets the same standards of quality, cleanliness, comfort and professionalism.`,
     homeSecondPartTitle:"The efficient management of passenger transportation via cabs is Taxi VIP St-Hubert's primary goal.",
     homeSecondPartText:`WHY TO BOOK WITH Taxi VIP St-Hubert 
-	- The largest fleet of minivans in the region: make your choice according to your needs.
-	- Online cab booking: in just a few clicks, order your cab using our mobile application. 
-	- Quick and easy payment: all our vehicles accept Interac direct payment cards. By opening a private or corporate account, you no longer need to pay each time you travel. The transportation service will be billed directly to your account. 
-	- Availability, speed and punctuality: trained and courteous, our drivers are available 24 hours a day, 7 days a week.
-	- Comfort and safety: all our vehicles are fully equipped to ensure the safety and comfort of our passengers. 
+	<br>- The largest fleet of minivans in the region: make your choice according to your needs.
+	<br>- Online cab booking: in just a few clicks, order your cab using our mobile application. 
+	<br>- Quick and easy payment: all our vehicles accept Interac direct payment cards. By opening a private or corporate account, you no longer need to pay each time you travel. The transportation service will be billed directly to your account. 
+	<br>- Availability, speed and punctuality: trained and courteous, our drivers are available 24 hours a day, 7 days a week.
+	<br>- Comfort and safety: all our vehicles are fully equipped to ensure the safety and comfort of our passengers. 
 	
-	Want to know how much your trip will cost? Take a look at our rates page to calculate. 
-	Contact us today to find out more about our services or to book your cab.`,
+	<br><br>Want to know how much your trip will cost? Take a look at our rates page to calculate. 
+	<br>Contact us today to find out more about our services or to book your cab.`,
     fixedPriceTitle:[" FROM ST-HUBERT AIRPORT TO (YUL) AIRPORT","FROM ST-HUBERT AIRPORT TO DOWNTOWN MONTREAL ","FROM ST-HUBERT CITY TO (YUL) AIRPORT","UNLOCK CAR DOOR","BATTERY BOOST"],
     fixedPrice:["Day time 65$<br> Night time 75$","Starting from 40$"],
     homeThirdPartTitle:"THE FOLLOWING ARE SOME FIXED PRICES",
@@ -31,14 +31,13 @@ var translateObject = {
    Nos chauffeurs sont spécialement qualifiés et suivent les mêmes normes de qualité, de propreté, de confort et de professionnalisme..`,
    homeSecondPartTitle:"La gestion efficace du transport de passagers par taxi est l'objectif principal de Taxi VIP St-Hubert.",
    homeSecondPartText:`POURQUOI RÉSERVER AVEC Taxi VIP St-Hubert. 
-   - La plus grande flotte de minivans de la région : faîtes votre choix en fonction de vos besoins. 
-   - Réservation d’un taxi en ligne : en quelques clics seulement, commandez votre taxi grâce à notre application mobile. 
-   - Paiement simple et rapide : tous nos véhicules acceptent les cartes de paiement direct Interac. En ouvrant un compte privé ou corporatif, vous n’avez plus besoin de payer à chacun de vos déplacements. Le service de transport sera directement facturé sur votre compte. 
-   - Disponibilité, rapidité et ponctualité : formés et courtois, nos chauffeurs sont disponibles 24 heures sur 24 et 7 jours sur 7. 
-   - Confort et sécurité : tous nos véhicules sont dotés des équipements indispensables pour assurer la sécurité et le confort de nos passagers. 
-   
-   Vous voulez prévoir le prix de votre déplacement? Jetez un coup d’œil à notre page tarifs pour en faire le calcul. 
-   Contactez-nous dès aujourd’hui pour en savoir plus sur nos services ou pour réserver votre taxi..`,
+  <br> - La plus grande flotte de minivans de la région : faîtes votre choix en fonction de vos besoins. 
+  <br> - Réservation d’un taxi en ligne : en quelques clics seulement, commandez votre taxi grâce à notre application mobile. 
+  <br> - Paiement simple et rapide : tous nos véhicules acceptent les cartes de paiement direct Interac. En ouvrant un compte privé ou corporatif, vous n’avez plus besoin de payer à chacun de vos déplacements. Le service de transport sera directement facturé sur votre compte. 
+  <br> - Disponibilité, rapidité et ponctualité : formés et courtois, nos chauffeurs sont disponibles 24 heures sur 24 et 7 jours sur 7. 
+  <br> - Confort et sécurité : tous nos véhicules sont dotés des équipements indispensables pour assurer la sécurité et le confort de nos passagers. 
+  <br><br>Vous voulez prévoir le prix de votre déplacement? Jetez un coup d’œil à notre page tarifs pour en faire le calcul. 
+  <br>Contactez-nous dès aujourd’hui pour en savoir plus sur nos services ou pour réserver votre taxi..`,
    fixedPriceTitle:[" DE L'AÉROPORT ST-HUBERT VERS L'AÉROPORT DE (YUL) ", " DE L'AÉROPORT ST-HUBERT VERS LE CENTRE-VILLE DE MONTRÉAL ", " DE VILLE ST-HUBERT VERS L'AÉROPORT (YUL) ", " DÉVERROUILLER D'PORTIÈRE ", " BATTERIE BOOSTAGE"],
    fixedPrice:["Jour  65$ <br>Nuit  75$","A partir de 40$."],
    homeThirdPartTitle:"VOICI QUELQUES PRIX FIXES",
